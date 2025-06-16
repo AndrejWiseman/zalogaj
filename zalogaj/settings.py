2b3dea9f6fe42154643000cccff00dd6b176edc7
Andrej Wiseman <533737@gmail.com>
2025-06-16 12:32:48 +0200

    dd

--- zalogaj/settings.py
+++ zalogaj/settings.py
@@ -29,12 +29,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'corsheaders',
+
     'jelovnik',
     'narudzbe',
     
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -144,3 +147,15 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    'https://zalogaj.vercel.app',
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://zalogaj.vercel.app',
+]
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SAMESITE = 'Lax'
